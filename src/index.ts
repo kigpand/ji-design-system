ade111f823a63a702f68ffc7d52c1c87eb1f8ddb
김지훈 <kigpand@gmail.com>
2024-08-24 20:56:57 +0900

    fix: input export 누락 코드 추가, label padding 값 추가

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { default as Button } from "./components/Buttons/Button";
+
+export { default as Input } from "./components/Inputs/Input";
+
+export { default as LabelInput } from "./components/Inputs/LabelInput";
